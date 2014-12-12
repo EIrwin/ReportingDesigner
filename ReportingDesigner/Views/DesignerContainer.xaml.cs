4a69541f163c666c3b482178ad10d0c5bda6802d
Eric Irwin <Eric.Irwin1124@gmail.com>
2014-12-11 21:44:53 -0700

    Added AddPageBeforePane and AddPageAfterPane

--- ReportingDesigner/Views/DesignerContainer.xaml.cs
+++ ReportingDesigner/Views/DesignerContainer.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System.IO;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 using Microsoft.Win32;
+using ReportingDesigner.Controls;
 using Telerik.Windows.Controls;
 using Size = System.Drawing.Size;
 
@@ -15,6 +17,24 @@ namespace ReportingDesigner.Views
         public DesignerContainer()
         {
             InitializeComponent();
+            InitializeNewPagePanes();
+        }
+
+        private void InitializeNewPagePanes()
+        {
+            //Grab Location Parameters
+            double x = DesignerCanvasShape.Position.X;
+            double y = DesignerCanvasShape.Position.Y;
+
+            //Position 'Add Page Before' Pane
+            AddPageBeforePane addPageBeforePane = new AddPageBeforePane();
+            addPageBeforePane.Position = new Point(x, y - 50);
+            Container.AddShape(addPageBeforePane);
+
+            //Position 'Add Page After' Pane
+            AddPageAfterPane addPageAfterPane = new AddPageAfterPane();
+            addPageAfterPane.Position = new Point(x, y + DesignerCanvasShape.Height + 25);
+            Container.AddShape(addPageAfterPane);
         }
 
         public void ToggleGridLines()
