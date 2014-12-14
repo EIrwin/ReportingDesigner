7d9188f4e774e77887e19ebfd6d97ad3cb40d5ce
Eric Irwin <Eric.Irwin1124@gmail.com>
2014-12-14 15:45:22 -0700

    Modified Window to open maximized and centered

---
+++ ReportingDesigner/Commands/AddPageAfter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace ReportingDesigner.Commands
+{
+    public class AddPageAfter:ICommand
+    {
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
--- ReportingDesigner/Views/DesignerContainer.xaml.cs
+++ ReportingDesigner/Views/DesignerContainer.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -14,6 +15,9 @@ namespace ReportingDesigner.Views
     /// </summary>
     public partial class DesignerContainer : RadDiagram
     {
+        private AddPageBeforePane _addPageBeforePane;
+        private AddPageAfterPane _addPageAfterPane;
+
         public DesignerContainer()
         {
             InitializeComponent();
@@ -26,15 +30,23 @@ namespace ReportingDesigner.Views
             double x = DesignerCanvasShape.Position.X;
             double y = DesignerCanvasShape.Position.Y;
 
+            //Check if pane already exists, remove if it does
+            if (Container.Shapes.Any(p => Equals(p, _addPageBeforePane)))
+                Container.RemoveShape(_addPageBeforePane);
+
+            //Check if pane already exists, remove if it does
+            if (Container.Shapes.Any(p => Equals(p, _addPageAfterPane)))
+                Container.RemoveShape(_addPageAfterPane);
+
             //Position 'Add Page Before' Pane
-            AddPageBeforePane addPageBeforePane = new AddPageBeforePane();
-            addPageBeforePane.Position = new Point(x, y - 50);
-            Container.AddShape(addPageBeforePane);
+            _addPageBeforePane = new AddPageBeforePane();
+            _addPageBeforePane.Position = new Point(x, y - 50);
+            Container.AddShape(_addPageBeforePane);
 
             //Position 'Add Page After' Pane
-            AddPageAfterPane addPageAfterPane = new AddPageAfterPane();
-            addPageAfterPane.Position = new Point(x, y + DesignerCanvasShape.Height + 25);
-            Container.AddShape(addPageAfterPane);
+            _addPageAfterPane = new AddPageAfterPane();
+            _addPageAfterPane.Position = new Point(x, y + DesignerCanvasShape.Height + 25);
+            Container.AddShape(_addPageAfterPane);
         }
 
         public void ToggleGridLines()
@@ -49,7 +61,11 @@ namespace ReportingDesigner.Views
 
         public void AddNewPage()
         {
-            
+            //For now we are just going to add a page to the end of the page
+            DesignerCanvasShape.Height = DesignerCanvasShape.Height*2;
+
+            //We need to reinitialize the add page panes
+            InitializeNewPagePanes();
         }
     }
 }
